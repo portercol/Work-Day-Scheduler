1861efd14a650e145f000ff36c42cf58006d6107
Collin Porter <colls.potta@gmail.com>
2020-08-20 16:19:30 -0600

    updated script file

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,23 @@
-//The first thing I want to do is populate the current date to the top of the page
+// Populate the current date to the top of the page
 
 var NowMoment = moment();
-var eDisplayMoment = document.getElementById('currentDay');
+var eDisplayMoment = document.getElementById('currentDay')
 eDisplayMoment.innerHTML = NowMoment.format('MMMM Do, YYYY');
+
+
+// Get the save button to save the message typed in the box
+$("#button-addon1").on("click", function(){
+    var textBox = localStorage.setItem("#form1", "Jibberish")
+    console.log("form1")
+})
+
+// (".saveBtn").onclick = event => {
+//     event.preventDefault();
+//     data = getFormData();
+//     localStorage.setItem(formIdentifier, JSON.stringify(data[formIdentifier]));
+//     const message = "Form draft has been saved!";
+//     displayAlert(message);
+//   };
+
+
 
